6a559f2fda965f3a0cd83c0f6f1b14ab6012e2ae
Himanshu2025 <hkulkarni927@gmail.com>
2024-08-02 09:31:29 +1000

    Initial commit with Myname.vue component

---
+++ src/components/Myname.vue
@@ -0,0 +1,18 @@
+<script setup>
+let name = "Himanshu"
+</script>
+
+<template>
+
+<h1> {{ name }}</h1>
+</template>
+
+<style scoped>
+
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+</style>
